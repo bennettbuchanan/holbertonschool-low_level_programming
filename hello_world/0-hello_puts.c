ab8174f210e8f5b8c9a8597bb3fc6f9a76e4c306
Bennett Buchanan <46@p4-3.hbtn.io>
2016-01-29 17:19:28 -0800

    add mandatory exercises

---
+++ hello_world/0-hello_puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  puts("Hello, C");
+  return (0);
+}
---
+++ hello_world/1-hello_Wall.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char str1[15];
+  strcpy(str1, "Hello, C");
+  puts(str1);
+  return (0);
+}
