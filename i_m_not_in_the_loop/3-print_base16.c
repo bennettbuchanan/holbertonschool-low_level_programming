4c383f63f5043877b8aacd17fb077a0ed3f3dd25
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-08 13:30:32 -0800

    remove blank lines and add comments

--- i_m_not_in_the_loop/3-print_base16.c
+++ i_m_not_in_the_loop/3-print_base16.c
@@ -1,21 +1,19 @@
 #include "my_functions.h"
-
 /* call print_alphabet function */
 void print_base16(void)
 {
   /* define variables */
   int letter = 48;
   int i;
-  
   /* initiate a loop to print entire alphabet */
   for (i = 0; i < 10; i++)
     {
       print_char(letter);
       letter++;
     }
-  
+  /* redefine letter to 65 */
   letter = 65;
-  
+  /* initiate a loop to print letters */
   for (i = 0; i < 6; i++)
     {
       print_char(letter);
