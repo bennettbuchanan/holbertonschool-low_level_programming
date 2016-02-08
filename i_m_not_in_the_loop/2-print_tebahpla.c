fd8560946b6498a13d224cb3dfecc92104d3e708
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-08 13:42:13 -0800

    add comment at print_char function

--- i_m_not_in_the_loop/2-print_tebahpla.c
+++ i_m_not_in_the_loop/2-print_tebahpla.c
@@ -10,6 +10,7 @@ void print_tebahpla(void)
   /* initiate a loop to print entire alphabet backwards */
   for (i = 0; i < 26; i++)
     {
+      /* call print_char function with variable as argument */
       print_char(letter);
       letter--;
     }
