1be587a6c87f8cd35caab40100cc761705a56c55
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-08 13:45:20 -0800

    ad

--- i_m_not_in_the_loop/4-positive_or_not.c
+++ i_m_not_in_the_loop/4-positive_or_not.c
@@ -2,21 +2,23 @@
 /* call positive_or_not function */
 void positive_or_not(int n)
 {
-  /* define variables */
   char P = 'P';
   char N = 'N';
   char Z = 'Z';
   /* create conditional statements to print P, N, or Z */
   if (n > 0)
     {
+      /* call print_char function with variable as argument */
       print_char(P);
     }
   if (n < 0)
     {
+      /* call print_char function with variable as argument */
       print_char(N);
     }
   if (n == 0)
     {
+      /* call print_char function with variable as argument */
       print_char(Z);
     }  
 }
