dfa29ad02ebf3260bfd091b8b1e8215d16017ab9
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-08 13:55:45 -0800

    substitute proper testing numbers

--- i_m_not_in_the_loop/4-main.c
+++ i_m_not_in_the_loop/4-main.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-  positive_or_not(-34298);
+  positive_or_not(98));
   positive_or_not(0);
-  positive_or_not(40232423);
-  positive_or_not(0);
+  positive_or_not(402);
+  positive_or_not(-459);
   print_char('\n');
   return (0);
 }
