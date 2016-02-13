60a5751de7e0cd38185e5781312ff6d25d435ea8
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-12 23:29:24 -0800

    add main files to compile

---
+++ dreams_and_dedication_are_a_powerful_combination/3-main.c
@@ -0,0 +1,8 @@
+#include "my_functions.h"
+
+int main(void)
+{
+  print_combination_3();
+  print_char('\n');
+  return (0);
+}
---
+++ dreams_and_dedication_are_a_powerful_combination/4-main.c
@@ -0,0 +1,8 @@
+#include "my_functions.h"
+
+int main(void)
+{
+  print_combination_4();
+  print_char('\n');
+  return (0);
+}
