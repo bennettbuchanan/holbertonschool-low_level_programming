79c8accbc6d62db49d416aadfafe0df6f404b3e2
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-14 17:52:06 -0800

    reduce length of print_combination_4 function

--- dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
+++ dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
@@ -4,34 +4,23 @@
 void print_combination_4(void)
 {
   int i, j, k, l;
-  /* first pair of numbers counts by one */
-  /* track the tenths place */
-  for (i = 0; i < 10; i++)
-    {
-      /* track the ones place */
+  for (i = 0; i < 10; i++)/* track the tenths place */
+    { /* track the ones place */
       for (j = 0; j < 10; j++)
-	{
-	  /* second pair of numbers relative to first pair */
+	{ /* second pair of numbers relative to first pair */
 	  for (k = i; k < 10; k++)
-	    {
-	      /* ones place initializes at var j + 1 */
+	    { /* ones place initializes at var j + 1 */
 	      for (l = j + 1; l < 10; l++)
-		{
-		  print_char(i + 48);
+		{ print_char(i + 48);
 		  print_char(j + 48);
 		  print_char(' ');
-
 		  print_char(k + 48);
 		  print_char(l + 48);
-		  /* do not print after first couple 98 */
-		  if (i == 9 && j == 8)
-		    {
-		    }
+		  if (i == 9 && j == 8)/* do not print after 98 */
+		    {}
 		  else
-		    {
-		      print_char(',');
-		      print_char(' ');		      
-		    }
+		    { print_char(',');
+		      print_char(' ');}
 		}
 	    }
 	}
