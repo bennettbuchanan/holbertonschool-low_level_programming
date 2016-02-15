b547d9f87b38c2d326b30f524fee109d8ff12d77
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-14 18:53:13 -0800

    reduce length of funtion below 25 lines

--- d/fizzbuzz/fizzbuzz.c
+++ d/fizzbuzz/fizzbuzz.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
- 
-int main()
+
+/* function to print numbers or words based on divisibility */
+int main(void)
 {
   int i;
-  for (i = 1; i < 101; i++)
-    {
-      if (i % 5 == 0 && i % 3 == 0)
-	{
-	  printf("FizzBuzz");
-	  printf(" ");  
+  for (i = 1; i < 101; i++) /* print numbers between 0-100 */
+    {if (i % 5 == 0 && i % 3 == 0) /* if i is divisible by 3 and 5 */
+	{printf("FizzBuzz");
+	 printf(" ");}
+      if (i % 5 == 0 && i % 3 != 0) /* if i is divisible by 5 */
+	{if (i == 100) /* if i is 100, do not add space after */
+	    {printf("Buzz");}
+	  else /* if i is not 100, add space after */
+	    {printf("Buzz");
+	     printf(" ");}
 	}
-      if (i % 5 == 0 && i % 3 != 0)
-	{
-	  if (i == 100)
-	    {
-	      printf("Buzz");
-	    }
-	  else
-	    {
-	      printf("Buzz");
-	      printf(" ");
-	    }
-	}
-      if (i % 3 == 0 && i % 5 != 0)
-	{
-	  printf("Fizz");
-	  printf(" ");
-	}     
-      if (i % 5 != 0 && i % 3 != 0)
-	{
-	  printf("%d", i);
-	  printf(" ");
-	}
+      if (i % 3 == 0 && i % 5 != 0) /* if i is divisible by 3 */
+	{printf("Fizz");
+	 printf(" ");}     
+      if (i % 5 != 0 && i % 3 != 0) /* otherwise, just print the number */
+	{printf("%d", i);
+	 printf(" ");}
     }
   return 0;
 }
