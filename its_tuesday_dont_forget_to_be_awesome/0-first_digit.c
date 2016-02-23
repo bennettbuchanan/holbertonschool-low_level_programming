e00192db2643087eced54fb0ed1a7d5f05127acc
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-02-23 11:25:31 -0800

    revise comments for first_digit function

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,4 +1,4 @@
-/* call print_number function to print each digit using print_char */
+/* call first_digit function to return the first digit of a number */
 int first_digit(int n)
 {
   int length = 0;
@@ -6,7 +6,7 @@ int first_digit(int n)
   int digit, length_stored, i;
   if (n == -2147483648 || n == 2147483647)
     {n = 2;}
-  if (n < 0 && n != -2147483648)/* print '-' and make n make positive*/
+  if (n < 0 && n != -2147483648)/* make n positive*/
     {n *= -1;}
   for (i = n; i > 0; i = i / 10)/* find the decimal length of n */
     {length++;}
@@ -15,7 +15,7 @@ int first_digit(int n)
     {for (i = 1; length_stored > i; i++)
 	{power *= 10;}
       digit = n / power; /* divide n by power to find the digit */
-      return(digit); /* print digit plus 48 on ascii */
+      return(digit); /* return the digit for main function */
       n = n - (digit * power);
       power = 1;
       length_stored--;}
