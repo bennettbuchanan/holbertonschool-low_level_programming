a80976bf8dcfa5134834e7f88276497b9bb6b7a2
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-17 19:48:51 -0700

    fix indentation

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
@@ -5,10 +5,10 @@ int strings_compare(char *s1, const char *s2)
   for ( ; s1[i] != '\0'; i++)
   {
   	if (s1[j] != s2[j]) /* if chars are different, break */
-	{
-	  break;
-	}
-	j++;
+	  {
+	    break;
+	  }
+	  j++;
   }
   return(s1[j] - s2[j]); /* return difference in chars */
 }
