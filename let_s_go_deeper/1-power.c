679364941b34b993361c1aaf7a3e568f12f89bc4
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-03-30 23:18:31 -0700

    handle int max when used as int x, and powers of 2

--- let_s_go_deeper/1-power.c
+++ let_s_go_deeper/1-power.c
@@ -1,21 +1,21 @@
 int power(int x, int y) {
-  long z;
-  if (y < 0 || x < 0) {
-    return -1;
-  }
+  long z, a;
+  a = x;
+  if (y<0||x<0||(a>46340&&y>1)||(x==4&&y>15)||(x==8&&y>10)||(x==16&&y>7)) {
+    return -1;}
+  if((x==32&&y>6)||(x==64&&y>5)||(x==128&&y>4)||(x==256&&y>3)||(x==512&&y>3)){
+    return -1;}
+  if((x==1024&&y>3)||(x % 2048 == 0 && y>2) ){ /* handle futher powers */
+    return -1;}
   if (y == 0) {
-    return 1;
-  }
+    return 1;}
   if (y == 1) {
-    return x;
-  }
+    return x;}
   y -= 1;
   z = x * power(x, y);
   if (z > 2147483647) {
-    return -1;
-  }
+    return -1;}
   if (z < 0) {
-    return -1;
-  }
+    return -1;}
   return x * power(x, y); /* recursively call the function */
 }
