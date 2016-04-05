793cb4c1c052e8aba45a76146786cbec7a3480b7
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-04-04 19:57:47 -0700

    add comments to script

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv) {
   int num_args;
   **argv = **argv;
   num_args = argc - 1;
+  /* print number of arguments given to program */
   printf("%d\n", num_args);
-	return (0);
+  return (0);
 }
--- neither_irony_or_sarcasm_is_argument/1-args.c
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv) {
   int i;
+  /* For each argument given to program, print to output */
   for (i = 0; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
