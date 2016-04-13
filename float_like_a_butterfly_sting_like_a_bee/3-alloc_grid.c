521471f85ae394544455ed9dec06cebbdca2acad
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-04-12 21:52:51 -0700

    change how much memory is allocated, add free function

--- float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height) {
     return (NULL);
   }
   for (i = 0; i < height; i++) {
-    grid[i] = malloc(sizeof(int));
+    grid[i] = malloc(sizeof(int) * height);
     if (grid[i] == NULL) {
       return (NULL);
     }
--- float_like_a_butterfly_sting_like_a_bee/4-free_grid.c
+++ float_like_a_butterfly_sting_like_a_bee/4-free_grid.c
@@ -1,30 +1,5 @@
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height) {
-  int i, j;
-  int **grid;
-
-  grid = malloc(sizeof(int **) * width);
-  if (grid == NULL) {
-    return (NULL);
-  }
-  for (i = 0; i < height; i++) {
-    grid[i] = malloc(sizeof(int));
-    if (grid[i] == NULL) {
-      return (NULL);
-    }
-  }
-  /* Assign decimals to the grid array */
-  for (i = 0; i < width; i++) {
-    for (j = 0; j < height; j++) {
-      grid[i][j] = 0;
-    }
-  }
-
-  return (grid);
-  free(grid);
-}
-
 void free_grid(int **grid, int x) {
   int i;
   for(i = 0; i < x; i++) {
