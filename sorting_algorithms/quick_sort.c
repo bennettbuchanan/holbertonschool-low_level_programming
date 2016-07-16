e6e0d383db38c1f86d50dd9513804984a104a824
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-07-16 16:47:26 -0700

    Add working quick sort algorithm.

--- sorting_algorithms/quick_sort.c
+++ sorting_algorithms/quick_sort.c
@@ -1,76 +1,74 @@
 #include <stdio.h>
 
 void print_array(int *, int);
-void move_pivot(int *arr, int i, int is_last);
+int move_pivot(int *arr, int i, int is_last);
 void recursive_call(int, int *, int);
 
 void quick_sort(int *arr, int len)
 {
         int i, j, tmp;
+        int pivot = 1;
 
         for (i = 1; i < len; ++i) {
-                /* Case where all elements are less than the pivot. */
-                if (i + 1 == len && arr[i] < arr[0]) {
-                        move_pivot(arr, i, 0);
-                        break;
-                }
-                if (arr[i] > arr[0]) {
-                        /* Case where all elements except the last are less
-                         * than the pivot element.
-                         */
-                        if (i == len - 1) {
-                                move_pivot(arr, i, 1);
-                                break;
-                        }
+                if (arr[i] >= arr[0]) {
                         for (j = i + 1; j < len; ++j) {
-                                while (arr[j] < arr[i] && arr[j] < arr[0]) {
+                                if (arr[j] < arr[i] && arr[j] < arr[0]) {
                                         /* Swap the values. */
                                         tmp = arr[i];
                                         arr[i] = arr[j];
                                         arr[j] = tmp;
-                                        ++j;
                                         ++i;
-                                        if (j == len) {
-                                                --j;
-                                                break;
-                                        }
                                 }
-                                if (j + 1 == len) {
-                                        /* Move pivot to center. */
-                                        move_pivot(arr, i, 1);
+                                if (j == len - 1) {
                                         break;
                                 }
                         }
-                        if (j + 1 == len) {
+                        if (j >= len - 1) {
+                                pivot = move_pivot(arr, i, 1);
                                 break;
                         }
                 }
+                /* All elements < pivot. */
+                if (i == len - 1 && arr[i] < arr[0]) {
+                        pivot = move_pivot(arr, i, 0);
+                } else if (i == len - 1 && arr[i] > arr[0]) {
+                        /* All elements except last are < pivot element. */
+                        pivot = move_pivot(arr, i, 1);
+                }
         }
-        recursive_call(i, arr, len);
+        if (pivot == 0) {
+                recursive_call(i, arr, len);
+        }
 }
 
 void recursive_call(int i, int *arr, int len)
 {
         /* Sort arrays recursively to the left. */
         if (i - 1 > 1) {
+                printf("\nleft: \n");
+                print_array(arr, i - 1);
                 quick_sort(arr, i - 1);
         }
         /* Sort arrays recursively to the right. */
         if (len - i > 1) {
+                printf("\nright: \n");
+                print_array(arr + i, len - i);
                 quick_sort(arr + i, len - i);
         }
 }
 
-void move_pivot(int *arr, int i, int is_last)
+int move_pivot(int *arr, int i, int is_last)
 {
         int tmp;
         if (is_last == 0) {
                 tmp = arr[0];
                 arr[0] = arr[i];
                 arr[i] = tmp;
-        } else {
+        }
+        if (is_last == 1) {
                 tmp = arr[0];
                 arr[0] = arr[i - 1];
                 arr[i - 1] = tmp;
         }
+        return 0;
 }
