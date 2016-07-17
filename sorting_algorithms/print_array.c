d7615fbc8fd1d4a048c92e5adc3ee0bb60f990ab
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-07-17 14:17:29 -0700

    Update print_num function to handle negative numbers, add documentation.

--- sorting_algorithms/print_array.c
+++ sorting_algorithms/print_array.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 void print_num(int);
@@ -6,20 +5,24 @@ void print_char(char);
 void print_str(char *str);
 void print_array_str(char **array);
 
+/* Print an array of ints to stdout. */
 void print_array(int *arr, int len)
 {
         int i;
 
         for (i = 0; i < len; ++i) {
                 if (i + 1 == len) {
-                        printf("%d", arr[i]);
+                        print_num(arr[i]);
                         break;
                 }
-                printf("%d, ", arr[i]);
+                print_num(arr[i]);
+                print_char(',');
+                print_char(' ');
         }
-        printf("\n");
+        print_char('\n');
 }
 
+/* Print an array of strings to stdout. */
 void print_array_str(char **array)
 {
         int i, j;
@@ -44,6 +47,10 @@ void print_array_str(char **array)
  */
 void print_num(int n)
 {
+        if (n < 0) {
+                n *= -1;
+                print_char('-');
+        }
         if (n >= 10) {
                 print_num(n / 10);
         }
